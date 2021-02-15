ef4225ff86818a4ae5e0e359a6abbd6ead0c4e13
ramazanozdemir <ramazanozdemir2607@gmail.com>
2021-02-15 17:57:48 +0300

    .

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -33,7 +33,7 @@ export function register(config) {
 
     window.addEventListener("load", () => {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-
+      console.log("swUrl --->", swUrl);
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
         checkValidServiceWorker(swUrl, config);
@@ -58,6 +58,7 @@ function registerValidSW(swUrl, config) {
   navigator.serviceWorker
     .register(swUrl)
     .then((registration) => {
+      console.log("register is complated");
       registration.onupdatefound = () => {
         const installingWorker = registration.installing;
         if (installingWorker == null) {
